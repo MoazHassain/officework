4db6abb8a4d0f1111f0175e1a67fe54260d4c55d
MoazHassain <moazhassain@gmail.com>
2021-02-22 11:56:45 +0600

    cart & favourite page

--- bag-n-brand/app/header-footer.js
+++ bag-n-brand/app/header-footer.js
@@ -59,3 +59,37 @@ accessLogin.addEventListener("click", function(){
     loginModalBg.style.display = "block";
     signupModalBg.style.display = "none";
 })
+
+var resetBtn = document.querySelector(".login-modal .reset a")
+var resetPassBg = document.querySelector(".reset-pass-bg")
+
+resetBtn.addEventListener("click", function(){
+    resetPassBg.style.display = "block";
+})
+
+var setResetCode = document.querySelector(".second-modal .set-reset-code a")
+var setNewpassBg = document.querySelector(".set-newpass-bg")
+
+setResetCode.addEventListener("click", function(){
+    setNewpassBg.style.display = "block";
+    resetPassBg.style.display = "none";
+})
+
+var getResetCode = document.querySelector(".second-modal .get-reset-code a")
+
+getResetCode.addEventListener("click", function(){
+    resetPassBg.style.display = "block";
+    setNewpassBg.style.display = "none";
+})
+
+window.addEventListener("click", function(e){
+    if(e.target == resetPassBg){
+        resetPassBg.style.display = "none";
+    }
+})
+
+window.addEventListener("click", function(e){
+    if(e.target == setNewpassBg){
+        setNewpassBg.style.display = "none";
+    }
+})
