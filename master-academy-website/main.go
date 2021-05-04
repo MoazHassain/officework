c4c24898fd94162860ef575241fede547655ece6
MoazHassain <moazhassain@gmail.com>
2021-05-04 06:08:33 +0600

    font style update

--- master-academy-website/main.go
+++ master-academy-website/main.go
@@ -10,6 +10,9 @@ func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/sign-up", signUp)
 	http.HandleFunc("/sign-in", signIn)
+	http.HandleFunc("/course-catalog", courseCatalog)
+	http.HandleFunc("/course-detail", courseDetail)
+	http.HandleFunc("/blog", blog)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8888", nil)
 }
@@ -49,6 +52,54 @@ func signIn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ptmp, err = ptmp.ParseFiles("wpage/sign-in.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+
+}
+
+func courseCatalog(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/course-catalog.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+
+}
+
+func courseDetail(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/course-detail.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+
+}
+
+func blog(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/blog.gohtml")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
